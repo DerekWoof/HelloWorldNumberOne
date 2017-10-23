175ec0989b131cd5d191fc45bf575d7090ce7f88
Your Name <you@example.com>
2017-10-23 18:50:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    private static String first = "Dzień dobry";
+
+    public static void main(String[] args) {
+
+        System.out.println(first + "\nHello World");
+
+    }
+}
